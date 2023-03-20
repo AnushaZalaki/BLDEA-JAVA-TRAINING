9a8eb0dfbd8db5c25f8d389474e339574a33b7c0
Anusha <Anusha@192.168.1.62>
2023-03-20 12:30:55 -0700

    Old Programs added

---
+++ src/P2/Pattern1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package P2;
+
+/**
+ * @author Anusha
+ *
+ */
+public class Pattern1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("hello");
+
+	}
+
+}
