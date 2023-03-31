f469c7661c734774e4cd271800ca841401b9e1a9
Anusha <Anusha@Rani>
2023-03-30 20:59:30 -0700

    programs on string

---
+++ src/P2/stringcod1.java
@@ -0,0 +1,11 @@
+package P2;
+
+public class stringcod1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Ramayana";
+		String s2=("Mahabharatha");
+	}
+
+}
